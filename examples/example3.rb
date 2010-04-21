7afc1b5d7c4e9dfa5968914b8e4b78ba89798af3
Jeremy Israelsen <jeremy.israelsen@leadmediapartners.com>
2010-04-21 00:26:46 -0600

    organized examples

---
+++ examples/example3.rb
@@ -0,0 +1,14 @@
+require File.dirname(__FILE__) + '/../init'
+
+model = ORM::Parser.parse(File.open(File.dirname(__FILE__) + "/example3.orm"))
+
+puts "Object Types:\n"
+model.object_types.map{|o| puts o.inspect }
+
+puts
+puts "Fact Types:\n"
+model.fact_types.map{|o| puts o.inspect }
+
+puts
+puts "Constraints:\n"
+model.constraints.map{|o| puts o.inspect }
